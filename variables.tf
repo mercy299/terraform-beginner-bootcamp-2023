578ab3b8507ae1215a3413e5c958f7983197f492
mercy299 <mercysam299@gmail.com>
2023-09-29 14:37:48 +0000

    bucket should no longer use random provider

--- variables.tf
+++ variables.tf
@@ -1,8 +1,19 @@
-variable "user_uuid"{
+variable "user_uuid" {
     description = "The UUID of the user"
     type = string
+    default     = "example-user-uuid"
     validation {
-      name = "is-valid-uuid"
-      condition = can(regex())
+      condition     = can(regex("^[a-z0-9.-]*$", var.user_uuid))
+      error_message = "User UUID must match the specified pattern."
     }   
+}
+
+variable "bucket_name" {
+  description = "The name of the AWS S3 bucket"
+  type        = string
+
+  validation {
+    condition     = can(regex("^[a-z0-9.-]*$", var.bucket_name))
+    error_message = "Invalid bucket name. Bucket names can only contain lowercase letters, numbers, hyphens, and periods."
+  }
 }
