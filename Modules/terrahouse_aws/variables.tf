9979bd64d3b6291652f494e2747bd0fb1ec1b7e6
mercy299 <mercysam299@gmail.com>
2023-09-30 06:52:44 +0000

    #27 refactor s3. bucket into the new terrahouse-aws modules

---
+++ Modules/terrahouse_aws/variables.tf
@@ -0,0 +1,21 @@
+variable "user_uuid" {
+    description = "The UUID of the user"
+    type = string
+    default     = "example-user-uuid"
+    validation {
+      condition     = can(regex("^[a-z0-9.-]*$", var.user_uuid))
+      error_message = "User UUID must match the specified pattern."
+    }   
+}
+
+variable "bucket_name" {
+  description = "The name of the AWS S3 bucket"
+  type        = string
+
+  validation {
+    condition     = can(regex("^[a-z0-9.-]*$", var.bucket_name))
+    error_message = "Invalid bucket name. Bucket names can only contain lowercase letters, numbers, hyphens, and periods."
+  }
+}
+
+# can(regex ("^[a-z0-9][a-z0-9]*[a-z0-9]$", var.bucket_name)) 
