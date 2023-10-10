65556dfe01d0118581e3803f34022c4bb2e367fd
mercy299 <mercysam299@gmail.com>
2023-10-10 09:15:44 +0000

    #42 make sure that all our endpoints work correctly

--- terratowns_mock_server/server.rb
+++ terratowns_mock_server/server.rb
@@ -214,10 +214,12 @@ class TerraTownsMockServer < Sinatra::Base
 
     home = Home.new
     home.town = $home[:town]
-    home.name = name
+    home.domain_name = $home[:domain]
+    home.name = name 
     home.description = description
     home.domain_name = domain_name
     home.content_version = content_version
+    binding.pry
 
     unless home.valid?
       error 422, home.errors.messages.to_json
@@ -233,13 +235,14 @@ class TerraTownsMockServer < Sinatra::Base
     puts "# delete - DELETE /api/homes/:uuid"
     content_type :json
 
-     # delete from mock database
     if params[:uuid] != $home[:uuid]
       error 404, "failed to find home with provided uuid and bearer token"
     end
 
+     # delete from mock database
+    uuid = $home [:uuid]
     $home = {}
-    { message: "House deleted successfully" }.to_json
+    { uuid: uuid }.to_json
   end
 end
 
